780ddf9db52f7c75f46cbda0b2d38f29294ce508
Aydeelauanda <156032159+Aydeelauanda@users.noreply.github.com>
2026-02-13 02:33:22 +0000

    fix: corrigindo caminho do index.tsx no html

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 // @ts-nocheck
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
+import App from '.src/App'
 import "./styles/globals.css"
 
 const rootElement = document.getElementById('root')
